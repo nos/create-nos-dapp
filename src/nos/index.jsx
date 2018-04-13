ff4408e1f003ef7e1fca92117c160b77e121a2ae
Jeroen Peeters <jeroen.peeters@evance.be>
2018-04-13 23:47:24 +0200

    feat(nos): add nOS HOC

---
+++ src/nos/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const nos = window.NOS && window.NOS.V1 ? window.NOS.V1 : {};
+
+const { Provider, Consumer } = React.createContext({
+  // Check whether nOS exists in your window
+  exists: !!window.NOS && !!window.NOS.V1,
+
+  // Wallet Actions
+  getAddress: () => nos.getAddress(),
+  getBalance: scriptHash => nos.getBalance(scriptHash),
+  claimGas: () => nos.claimGas(),
+  send: (asset, amount, receiver) => nos.send(asset, amount, receiver),
+
+  // Smart Contract Actions
+  testInvoke: (scriptHash, operation, args) =>
+    nos.testInvoke(scriptHash, operation, args),
+  invoke: (scriptHash, operation, args) =>
+    nos.invoke(scriptHash, operation, args),
+  getStorage: ({ scriptHash, key, encode = true }) =>
+    nos.getStorage(scriptHash, key, { encode })
+});
+
+const injectNOS = Component => props => (
+  <Consumer>{context => <Component nos={context} {...props} />}</Consumer>
+);
+
+export { Provider, Consumer, injectNOS };
